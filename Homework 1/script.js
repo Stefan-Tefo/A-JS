072118c7bb2c377d3cefe1c70481a995ad9e03df
Stefan-Tefo <stefan.trajkovski1@outlook.com>
2024-01-24 23:35:01 +0100

    Homework

--- Homework 1/script.js
+++ Homework 1/script.js
@@ -1,65 +1,42 @@
-let tbody = document.getElementById("tbody")            //.style.display = "none";
-
-let type = document.getElementById('t-type')
-let brand = document.getElementById('b-brand')
-
-let btnOne = document.getElementById('btn-1')
-let table = document.getElementById('table')
-let carsData = null;
-
-fetch("https://raw.githubusercontent.com/sedc-codecademy/mkwd12-04-ajs/main/G1/HOMEWORK/cars.json")
-    .then(function (res) {
-        return res.json()
-    })
-    .then(function (body) {
-        console.log(body);
-        carsData = body
-        // showCars(carsData)
-        /* 
-            Испраќаш аргументи во повикот на двете функции - showTypeOfCars и showBrandOfCars,
-            но во дефиницијата на функцијата не очекуваш аргументи. 
-            Прочитај го коментарот на 50та линија, од иста природа е.
-        */
-        showTypeOfCars(carsData)
-        showBrandOfCars(carsData)
-
-    })
-    .catch(function (error) {
-        console.log("ERROR", error);
-    })
-    .finally(function () {
-        console.log("Success");
-    });
-
-
-btnOne.addEventListener('click', function () {
-
-    // let value = type.value
-    // if (!value) {
-    //     alert("Enter a right option")
-    // }
-    showTableFromTheOptions()
-})
-
-function showTableFromTheOptions() {
-    /* Земи ја вредноста на опцијата која е селетирана, стави debugger за да видиш што вредност има type (https://stackoverflow.com/a/1085810) */
-    if (type === 'SUV') {
-        /* 
-            Очекуваш аргумент во дефиницијата на функцијата showCars, но овде не испраќаш аргументи.
-            Но, размисли, дали имаме потреба да ја испраќаме carsData како аргумент ако е глобална варијабла?
-            Дали имаме пристап до глобални варијабли низ функциите?
-        */
-        showCars() 
-    }
+const html = {
+    tbody: document.querySelector("#tbody"),
+    typeOfCar: document.querySelector("#typeOfCar"),
+    brandOfCar: document.querySelector("#brandOfCar"),
+    btnOne: document.querySelector("#btnOne"),
+    table: document.querySelector("#table"),
 }
 
-function showCars(carsData) {
+function showAllCars() {
+    fetch("https://raw.githubusercontent.com/sedc-codecademy/mkwd12-04-ajs/main/G1/HOMEWORK/cars.json")
+        .then((res) => res.json())
+        .then((car) => {
+            showCars(car)
+            showBrandOfCars(car)
+            showTypeOfCars(car)
+        })
+        .catch((error) => console.log("ERROR", error))
+        .finally(() => console.log("Success"));
+}
 
-    // tbody.innerHTML = ""
-    // if (!carsData) return;
+html.btnOne.addEventListener('click', () => {
+    const value = html.typeOfCar.value;
+    const value1 = html.brandOfCar.value;
+    html.tbody.innerHTML = '';
+    fetch("https://raw.githubusercontent.com/sedc-codecademy/mkwd12-04-ajs/main/G1/HOMEWORK/cars.json")
+        .then((res) => res.json())
+        .then((car) => {
 
-    for (let car of carsData) {
-        tbody.innerHTML += `
+        })
+})
+
+function showCars(car) {
+    car.forEach((car) => {
+        html.tbody.innerHTML += createTable(car)
+    });
+}
+
+function createTable(car) {
+    return `
     <tr>
     <td>${car.type}</td>
     <td>${car.brand}</td>
@@ -69,20 +46,19 @@ function showCars(carsData) {
     <td>${car.color}</td>
     <td>${car.isNew}</td>
     <td>${car.horsepower}</td>
-    </tr>
-    `
-    }
+    </tr> `
 }
 
-function showTypeOfCars() {
-    for (let car of carsData) {
-        type.innerHTML += `<option>${car.type}</option>`
-    }
+function showTypeOfCars(car) {
+    car.forEach((car) => {
+        html.typeOfCar.innerHTML += `<option>${car.type}</option>`;
+    })
 }
 
-function showBrandOfCars() {
-    for (let car of carsData) {
-        brand.innerHTML += `<option>${car.brand}</option>`
-    }
+function showBrandOfCars(car) {
+    car.forEach((car) => {
+        html.brandOfCar.innerHTML += `<option>${car.brand}</option>`;
+    })
 }
 
+showAllCars()
