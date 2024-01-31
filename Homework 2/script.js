1027994daf247e8e0d298932695c9bf2114f82e4
Stefan-Tefo <150601798+Stefan-Tefo@users.noreply.github.com>
2024-01-31 13:57:45 +0100

    Homework

--- Homework 2/script.js
+++ Homework 2/script.js
@@ -1,14 +1,14 @@
 html = {
-    imageContainer: document.querySelector(".imageContainer")
+    imageContainer: document.querySelector(".imageContainer"),
+    btnOne: document.querySelector("#btnOne")
 }
 
 function ranodomBeer() {
-    fetch("https://api.punkapi.com/v2/beers/random")
+    fetch("https://api.punkapi.com/v2/beers/1")
         .then((res) => res.json())
         .then((beer) => {
-            showImage(beer)
             console.log(beer);
-            console.log(showImage(beer))
+            showImage(beer)
         })
         .catch((error) => console.log("ERROR", error))
 }
@@ -18,9 +18,20 @@ ranodomBeer()
 function showImage(beer) {
     return html.imageContainer.innerHTML = `
     <div class="image">
-    <img src="${beer[0].image_url}" alt="${beer[0].name}" />
-    <p></p>
-    <p></p>
+    <h3>Random generate beer</h3>
+    <img src="${beer[0].image_url || "N/A"}" alt="${beer[0].name}" />
+    <p>${beer[0].abv}%</p>
+    <p>${beer[0].food_pairing}</p>
     </div>
     `
+}
+
+html.btnOne.addEventListener('click', () => GenerateRandomBear())
+
+function GenerateRandomBear() {
+    fetch("https://api.punkapi.com/v2/beers/random")
+    .then((res)=>res.json())
+    .then((beer)=>{
+        showImage(beer)
+    })
 }
