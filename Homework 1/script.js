c7bfaaf2553eb8c77ce1a82fda73a2b1e034427f
Stefan-Tefo <stefan.trajkovski1@outlook.com>
2024-02-01 15:32:55 +0100

    Homework

--- Homework 1/script.js
+++ Homework 1/script.js
@@ -6,6 +6,11 @@ const html = {
     table: document.querySelector("#table"),
     modelInput: document.querySelector("#modelInput"),
     resetBtn: document.querySelector("#resetBtn"),
+    numberOfDoor: document.querySelector("#numberOfDoor"),
+    gasType: document.querySelector("#gasType"),
+    newCar: document.querySelector("#newCar"),
+    oldCar: document.querySelector("#carColor")
+
 }
 
 function showAllCars() {
@@ -15,16 +20,19 @@ function showAllCars() {
             showCars(car)
             showBrandOfCars(car)
             showTypeOfCars(car)
+
         })
         .catch((error) => console.log("ERROR", error))
         .finally(() => console.log("Success"));
 }
 
 html.resetBtn.addEventListener("click", () => {
-    html.typeOfCar.value = ""
-    html.brandOfCar.value = ""
-    html.modelInput.value = ""
-    showAllCars()
+    html.typeOfCar.value = "";
+    html.brandOfCar.value = "";
+    html.modelInput.value = "";
+    html.numberOfDoor.value = "";
+    html.gasType.value = "";
+    html.carColor.value = "";
 })
 
 html.btnOne.addEventListener('click', () => {
@@ -32,19 +40,23 @@ html.btnOne.addEventListener('click', () => {
     const value = html.typeOfCar.value;
     const value1 = html.brandOfCar.value;
     const value2 = html.modelInput.value;
-    if (!value || !value1 || !value2) {
-        alert("Invalid input")
-        showAllCars()
-    }
-
+    const value3 = html.numberOfDoor.value;
+    const value4 = html.gasType.value;
+    // const value5 = html.carColor.value;
+    const value6 = html.newCar.value;
+    const value7 = html.oldCar.value;
     html.tbody.innerHTML = '';
     fetch("https://raw.githubusercontent.com/sedc-codecademy/mkwd12-04-ajs/main/G1/HOMEWORK/cars.json")
         .then((res) => res.json())
         .then((car) => {
             debugger
+            console.log(value5);
             showCars(car.filter((car) => car.type === value))
             showCars(car.filter((car) => car.brand === value1))
             showCars(car.filter((car) => car.model.toLowerCase() === value2))
+            showCars(car.filter((car) => car.doors === parseInt(value3)))
+            showCars(car.filter((car) => car.gasType === value4))
+            // showCars(car.filter((car) => car.color === value5))
         })
 })
 
@@ -81,6 +93,7 @@ function showBrandOfCars(car) {
         html.brandOfCar.innerHTML += `<option>${car.brand}</option>`;
     })
 }
+
 
 showAllCars()
 
