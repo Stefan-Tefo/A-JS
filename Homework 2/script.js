e7e9a7be77146cb2aa30fc735afa75fa4c4378ed
Stefan-Tefo <150601798+Stefan-Tefo@users.noreply.github.com>
2024-02-07 18:38:30 +0100

    Homework

--- Homework 2/script.js
+++ Homework 2/script.js
@@ -2,21 +2,29 @@ html = {
     imageContainer: document.querySelector(".imageContainer"),
     btnOne: document.querySelector("#btnOne"),
     btnGenerate: document.querySelector("#btn_generate"),
-  
+
 }
 
 function ranodomBeer() {
     fetch("https://api.punkapi.com/v2/beers/random")
         .then((res) => res.json())
         .then((body) => {
-            showImage(body)
+            const malt = getMaltNames(body[0].ingredients?.malt)
+            showImage(body, malt)
         })
         .catch((error) => console.log("ERROR", error))
 }
 
+// Get all malt names from the malt array 
+function getMaltNames(malt) {
+    if (malt) {
+        return malt.map(el => el.name).join(', ');
+    } else return 'No malt ingredients';
+}
+
 ranodomBeer()
 
-function showImage(beer) {
+function showImage(beer, malt) {
     return html.imageContainer.innerHTML = `
     <div class="beer">
         <div id="first_beer_name">
@@ -29,7 +37,7 @@ function showImage(beer) {
                 <p><img src="./icons/procente.png">ABV:${beer[0].abv}%</p>
                 <p><img src="./icons/cutlery.png">Food:${beer[0].food_pairing}</p>
                 <p><img src="./icons/bitter.png">IBU's:${beer[0].ibu}</p>
-                <p><img src="./icons/barley.png">Malts:${beer[0].ingredients.malt.name}</p>
+                <p><img src="./icons/barley.png">Malts:${malt}</p>
                 <p><img src="./icons/hop.png">Hops:${beer[0].ingredients.hops.name}</p>                
             </div>
         </div>
