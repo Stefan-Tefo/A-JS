670d1a37d54b5e3d571f7af9eee437102d3decd7
Stefan-Tefo <stefan.trajkovski1@outlook.com>
2024-02-10 14:30:12 +0100

    Homework

--- Homework 2/beer.js
+++ Homework 2/beer.js
@@ -2,17 +2,59 @@ html = {
     container: document.querySelector(".container"),
     pagination: document.querySelector("#pagination"),
     search: document.querySelector("#search"),
-    abv: document.querySelector("#abv")
+    abv: document.querySelector("#abv"),
+    srmLight: document.querySelector("#srmLight"),
+    srmMedium: document.querySelector("#srmMedium"),
+    srmDark: document.querySelector("#srmDark"),
 }
 
 html.abv.addEventListener("input", () => {
-    const isBelow = html.abv.checked                       
+    const isBelow = html.abv.checked
     fetch(`https://api.punkapi.com/v2/beers`)
         .then(res => res.json())
         .then((body) => {
             html.container.innerHTML = ""
             if (isBelow) {
                 showBeers(body = body.filter(body => body.abv >= 6));
+            }
+        })
+        .catch((error) => console.log("ERROR", error))
+})
+
+html.srmLight.addEventListener("input", () => {
+    const light = html.srmLight.checked
+    fetch(`https://api.punkapi.com/v2/beers`)
+        .then(res => res.json())
+        .then((body) => {
+            html.container.innerHTML = ""
+            if (light) {
+                showBeers(body = body.filter(body => body.srm >= 6));
+            }
+        })
+        .catch((error) => console.log("ERROR", error))
+})
+
+html.srmMedium.addEventListener("input", () => {
+    const medium = html.srmMedium.checked
+    fetch(`https://api.punkapi.com/v2/beers`)
+        .then(res => res.json())
+        .then((body) => {
+            html.container.innerHTML = ""
+            if (medium) {
+                showBeers(body = body.filter(body => body.srm <= 9 || body.srm >= 20));
+            }
+        })
+        .catch((error) => console.log("ERROR", error))
+})
+
+html.srmDark.addEventListener("input", () => {
+    const dark = html.srmDark.checked
+    fetch(`https://api.punkapi.com/v2/beers`)
+        .then(res => res.json())
+        .then((body) => {
+            html.container.innerHTML = ""
+            if (dark) {
+                showBeers(body = body.filter(body => body.srm >= 12 || body.srm >= 40));
             }
         })
         .catch((error) => console.log("ERROR", error))
